52739192a1973c2ccad44512e087df7d7b0c3fdf
药要吃 <wangrui19970405@gmail.com>
2023-12-06 21:13:51 +0800

    deploy:更新说明

--- packages/organization/src/index.vue
+++ packages/organization/src/index.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="organization-box">
-  
+  <div class="depart">1</div>
+  <div class="user-list"></div>
 </div>
 </template>
 
@@ -9,5 +10,14 @@
 </script>
 
 <style scoped lang="scss">
-
+.organization-box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  .depart {
+    width: 20%;
+    height: 100%;
+    min-width: 200px;
+  }
+}
 </style>
